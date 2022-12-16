f896d814e1145fbf8f2a97a68598f52a58d22f73
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-12-16 15:11:32 +0100

    print card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,22 @@
 </head>
 
 <body>
-    @foreach ($movies as $movie)
-    @endforeach
+    <div class="container d-flex flex-wrap">
+        @foreach ($movies as $movie)
+            <div class="card col-3" style="">
+                <img class="card-img-top" src="{{ $movie['image'] }}" alt="{{ $movie['title'] }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie['title'] }}</h5>
+                    <p class="card-text">titolo originale : {{ $movie['original_title'] }}</p>
+                    <p class="card-text"> data di uscita: {{ $movie['date'] }}</p>
+
+                    <p class="card-text"> voto: {{ $movie['vote'] }}</p>
+
+                </div>
+            </div>
+        @endforeach
+
+    </div>
 
 </body>
 
